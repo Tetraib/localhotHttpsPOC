3755b1d6ca5b27bda36441a88f087183def08131
Thibaut Constant <thibaut.constant@isisphinx.com>
2019-07-19 15:22:48 +0200

    format

--- main.go
+++ main.go
@@ -8,14 +8,11 @@ import (
 )
 
 func main() {
-
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"https://eager-boyd-0fa197.netlify.com"},
 	})
-
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Localhost called")
 	})
-
 	http.ListenAndServe(":8080", c.Handler(handler))
 }
